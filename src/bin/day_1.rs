84cb8f0d9eb4a453ab2ae41991a4b64ae226b38d
Elisabeth S. Friedrich <elisabeth.s.friedrich@gmail.com>
2021-12-10 19:13:41 +0100

    Add: Basic structure for day
    Compiles and runs (currently without logic)

---
+++ src/bin/day_1.rs
@@ -0,0 +1,42 @@
+//! The starter code slowly blinks the LED, and sets up
+//! USB logging.
+
+#![feature(alloc_error_handler)]
+
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use teensy4_panic as _;
+use cortex_m_rt::entry;
+
+use alloc::string::String;
+
+use aoc21::utils::Hardware;
+
+
+#[entry]
+fn wrapper() -> ! {
+    main();
+}
+
+fn main() -> ! {
+    let mut sol = Solution {};
+    aoc21::runtime::run(&mut sol);
+}
+
+struct Solution {
+}
+
+impl aoc21::solutions::Solution<u32> for Solution {
+    fn parse_file(&self, hardware: &Hardware, in_data: String) -> u32 {
+        return 5;
+    }
+
+    fn part_a(&self, hardware: &Hardware, data: &u32) {
+    }
+
+    fn part_b(&self, hardware: &Hardware, data: &u32) {
+    }
+}
