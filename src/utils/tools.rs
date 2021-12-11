29560df6ddead35d1f82dbec6b9d695aee9311f3
Elisabeth S. Friedrich <elisabeth.s.friedrich@gmail.com>
2021-12-11 12:17:08 +0100

    Add radix fnc to parse with err

--- src/utils/tools.rs
+++ src/utils/tools.rs
@@ -5,11 +5,16 @@ use num_traits::Num;
 use num_traits::bounds::Bounded;
 
 pub fn parse_with_err<T>(hardware: &mut Hardware, v: &str) -> T where T: Num + Bounded {
-    match T::from_str_radix(v, 10) {
+    return parse_with_err_radix(hardware, v, 10);
+}
+
+pub fn parse_with_err_radix<T>(hardware: &mut Hardware, v: &str, radix: u32) -> T where T: Num + Bounded {
+    match T::from_str_radix(v, radix) {
         Ok(obj) => obj,
         Err(_) => {
             writeln!(hardware.writer, "ERR: parsing string into num '{}'", v).unwrap();
             T::max_value()
         }
     }
+
 }
